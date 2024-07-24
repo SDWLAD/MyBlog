4920b7723d82e5077653f99eb961885acf8fefbe
SDWLAD <sidorchukvladislav0@gmail.com>
2024-07-24 23:49:49 +0300

    Added page "create article"

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:id>', views.article, name='article'),
+    path('create-article', views.create_article, name='create-article'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, redirect
 from . import models
+from .forms import PostForm
 
 def index(request):
-
     context = {
         "articles":
             models.Post.objects.all()
@@ -14,3 +14,13 @@ def index(request):
 def article(request, id):
     context = {"article":models.Post.objects.get(pk=id)}
     return render(request, 'article.html', context=context)
+
+def create_article(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+    return render(request, 'create_article.html', {'form': form})
