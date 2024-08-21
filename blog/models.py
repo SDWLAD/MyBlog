becc9ce1a2a61c5597a91c69bb07df764cf37a39
SDWLAD <sidorchukvladislav0@gmail.com>
2024-08-21 22:36:34 +0300

    added view counter placeholder

--- blog/models.py
+++ blog/models.py
@@ -7,6 +7,7 @@ class Post(models.Model):
     content = models.TextField()
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
+    views_count = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
